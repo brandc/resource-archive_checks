4d8d2f49a949f2305bf3f3d18d9c561ad7aa44e3
Brandc <brandc@users.noreply.github.com>
2016-05-10 01:14:42 -0700

    Completed the half finished code I thought was completed

--- ArchiveXMLcheck.py
+++ ArchiveXMLcheck.py
@@ -1,103 +1,121 @@
-import sys, os
-import xml.etree.ElementTree as ET
+import binascii
+import stat, os
+import getopt, sys
 import zlib, hashlib
+import xml.etree.ElementTree as ET
 
+def iterfile(f, bufSize):
+	while True:
+		data = f.read(bufSize)
+		yield data
+		if len(data) < bufSize:
+			return
 
-def XML2Array(filename):
-  tree = ET.parse(filename)
-  root = tree.getroot()
-  a = []
-  for f in root.findall('file'):
-    d = {}
-    try:
-      d['name'] = f.get('name')
-      d['size'] = int(f.find('size').text)
-      d['md5']  = f.find('md5').text
-      d['crc32'] = f.find('crc32').text
-      d['sha1'] = f.find('sha1').text
-      a.append(d)
-    except:
-      continue
-  return adef confirmFiles(files):
-  absent = []
-  broken = []
-  confirmed = []
-  for f in files:
-    try:
-      if f['size'] == os.stat(f['name']).st_size:
-        confirmed.append(f)
-      else:
-        broken.append(f)
-    except:
-      absent.append(f)
-  return (absent, broken, confirmed)
+def crc32(f, bufSize):
+	value = 0
+	if not (bufSize):
+		value = zlib.crc32(f.read()) & 0xffffffff
+	else:
+		for data in iterfile(f, bufSize):
+			value = zlib.crc32(data, value) & 0xffffffff
 
-# If you put false for any of the hashes, the default value will be returned.
-def checksumFile(f, bufsize, CRC32, MD5, SHA1):
-  if bufsize < 1:
-    bufsize = -1
-  try:
-    fi = open(f['name'], 'rb')
-  except:
-    print("Failed to open %s" % f['name'])
+	return int(value)
 
-  c = 0
-  m = hashlib.md5()
-  s = hashlib.sha1()
+def md5(f, bufSize):
+	hashobj = hashlib.md5()
 
-  while (True):
-    buf = fi.read(bufsize)
-    if len(buf) == 0:
-      break
-    else:
-      if CRC32 == True:
-        c = zlib.crc32(buf, c)
-      if MD5 == True:
-        m.update(buf)
-      if SHA1 == True:
-        s.update(buf)
+	if not (bufSize):
+		hashobj.update(f.read())
+	else:
+		for data in iterfile(f, bufSize):
+			hashobj.update(data)
 
-  fi.close()
-  c = hex(c)
-  c = c[2:]
-  return (c, m.hexdigest(), s.hexdigest())
+	return hashobj.digest()
 
-# essentially the main() function
-if __name__ == "__main__":
-  if len(sys.argv) != 2:
-    print("Requires XML input file.")
+def sha1(f, bufSize):
+	hashobj = hashlib.sha1()
 
-  files = XML2Array(sys.argv[1])
-  (absent, broken, confirmed) = confirmFiles(files)
-  print("Size matched files:")
-  print(" Confirmed: %4d" % len(confirmed))
-  print(" Absent:    %4d" % len(absent))
-  print(" Broken:    %4d" % len(broken))
-  passed = []
-  failed = []
-  for f in confirmed:
-    (CRC32, MD5, SHA1) = checksumFile(f, -1, True, True, True)
+	if not (bufSize):
+		hashobj.update(f.read())
+	else:
+		for data in iterfile(f, bufSize):
+			hashobj.update(data)
 
-    if (CRC32 == f['crc32']) and (MD5 == f['md5']) and (SHA1 == f['sha1']):
-      print("PASSED: %s" % f['name'])
-      passed.append(f)
-    else:
-      print("FAILED: %s" % f['name'])
-      failed.append(f)
+	return hashobj.digest()
 
+if (__name__ == "__main__"):
+	try:
+		longopts = ["crc32", "md5", "sha1"]
+		opts, args = getopt.getopt(sys.argv[1:], "i:b:", longopts)
+	except getopt.GetoptError as error:
+		print(error)
+		# Put in some usage function when the code is done
+		sys.exit(1)
 
+	bufSize = 0
+	infile = None
+	hashFunc = sha1
+	hashName = "sha1"
+	for (opt, arg) in opts:
+		if (opt == "--crc32"):
+			hashName = "crc32"
+			hashFunc = crc32
+		elif (opt == "--md5"):
+			hashName = "md5"
+			hashFunc = md5
+		elif (opt == "--sha1"):
+			hashName = "sha1"
+			hashFunc = sha1
+		elif (opt == "-i"):
+			infile = arg
+		elif (opt == "-b"):
+			bufSize = int(arg)
+			if (bufSize < 0):
+				print("-b must be zero or greater.")
+				sys.exit(1)
+		else:
+			print("Unknown argument: %s" % opt)
+			sys.exit(1)
 
+	if (infile == None):
+		print("Input file must be specified")
+		sys.exit(1)
+	else:
+		archiveXMLtree = ET.parse(infile)
+		root = archiveXMLtree.getroot()
 
+	for elem in root.findall('file'):
+		if (elem == None):
+			print(elem)
+			break
+		name = elem.get('name')
 
+		try:
+			ret = elem.find('size')
+			size = int(ret.text)
+			try:
+				fsize = os.stat(name).st_size
+			except FileNotFoundError:
+				print("DOES NOT EXIST: %s" % name)
+				continue
+		except AttributeError:
+			continue
 
+		textbin = elem.find(hashName).text
+		hashbin = binascii.unhexlify(textbin)
 
+		if (fsize != size):
+			print("Mismatched file size on: %s" % name)
+			continue
 
-
-
-
-
-
-
-
-
-
+		with open(name, "rb") as f:
+			fhash = hashFunc(f, bufSize)
+			if (hashName == "crc32"):
+				if (fhash != int(textbin, 16)):
+					print("BAD HASH: %s" % name)
+				else:
+					print("VERFIED: %s" % name)
+			elif (fhash != hashbin):
+				print("BAD HASH: %s" % name)
+			else:
+				print("VERFIED: %s" % name)
